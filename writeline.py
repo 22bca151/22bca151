a04b65f65c2c7b9c04fcf98e4b57776bc6736f74
krishnapatel04 <139547347+krishnapatel04@users.noreply.github.com>
2023-08-18 14:38:28 +0530

    Add files via upload

---
+++ writeline.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import csv
+
+
+# In[9]:
+
+
+f=open("c:\sqlite3\csv\kishu.txt","w")
+l=["shree \n","ram \n"]
+
+
+# In[10]:
+
+
+f.write("krishna\n")
+f.writelines(l)
+f.close()
+
+
+# In[ ]:
+
+
+
+
